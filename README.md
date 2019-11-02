9db07db001f838b91e4e559a78de87774a9b1298
Gaelle Marais <marais.gaelle@gmail.com>
2019-11-02 14:56:22 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# neural-network-language-detection
+
+A neural network able to detect the language of a text, using the FANN library. 
+
+
+## Compilation
+```
+$ gcc train.c -lfann -lm -o train.o
+$ gcc execute.c -lfann -lm -o execute.o
+```
---
+++ src/train.c
@@ -0,0 +1,27 @@
+#include "../include/fann.h"
+
+int main(){
+    const unsigned int num_input = 2;
+    const unsigned int num_output = 1;
+    const unsigned int num_layers = 3;
+    const unsigned int num_neurons_hidden = 3;
+    const unsigned int connection_rate = 1;
+    const float learning_rate = 0.5;
+    const float desired_error = (const float) 0.001;
+    const unsigned int max_epochs = 500000;
+    const unsigned int epochs_between_reports = 1000;
+    int layers[3] = {num_input, num_neurons_hidden, num_output};
+
+    struct fann *ann = fann_create_sparse_array(connection_rate, num_layers, layers);
+
+    fann_set_learning_rate(ann, learning_rate);
+
+    fann_train_on_file(ann, "../data/language.data", max_epochs, epochs_between_reports, desired_error);
+
+    fann_save(ann, "language.net");
+
+    fann_destroy(ann);
+
+    return 0;
+    
+}
